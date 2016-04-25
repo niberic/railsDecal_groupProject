259849fe6f74dfa2c7e1b0f98b41609cef366327
smuppalla <smuppalla@berkeley.edu>
2016-04-25 03:45:09 +0000

    added routes for project

--- when3meet/config/routes.rb
+++ when3meet/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#home'
   devise_for :users
   resources :events
-
+  resources :projects
   resources :time_blocks
 
   # The priority is based upon order of creation: first created -> highest priority.
--- when3meet/db/schema.rb
+++ when3meet/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160424235448) do
+ActiveRecord::Schema.define(version: 20160425032707) do
 
   create_table "events", force: :cascade do |t|
     t.string   "event_name"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 20160424235448) do
     t.datetime "updated_at",        null: false
     t.date     "begin"
     t.date     "end"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "time_blocks", force: :cascade do |t|
