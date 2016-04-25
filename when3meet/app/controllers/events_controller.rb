0dff16b329690b7f9c9863f3a1914923bf7463dd
Raul Puri <raulpuric@berkeley.edu>
2016-04-25 02:16:06 +0000

    only show events of logged in person

--- when3meet/app/controllers/events_controller.rb
+++ when3meet/app/controllers/events_controller.rb
@@ -4,7 +4,8 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    curruser=current_user()
+    @events = curruser.events
   end
 
   # GET /events/1
