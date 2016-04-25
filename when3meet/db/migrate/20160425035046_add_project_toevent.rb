51f1293102a7cd49c1133f233944b199cc3bd5e9
smuppalla <smuppalla@berkeley.edu>
2016-04-25 03:51:40 +0000

    new relationship

---
+++ when3meet/db/migrate/20160425035046_add_project_toevent.rb
@@ -0,0 +1,4 @@
+class AddProjectToevent < ActiveRecord::Migration
+  def change
+  end
+end
--- when3meet/db/schema.rb
+++ when3meet/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160425032707) do
+ActiveRecord::Schema.define(version: 20160425035046) do
 
   create_table "events", force: :cascade do |t|
     t.string   "event_name"
