fd6e63bba0c7ea5fdb96e050cac1184cc7d92970
smuppalla <smuppalla@berkeley.edu>
2016-04-25 05:01:56 +0000

    fixed title and seed

--- when3meet/db/seeds.rb
+++ when3meet/db/seeds.rb
@@ -2,6 +2,11 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
-#
+# => 
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Pokemon.create name: name, level: rand(1..20), health: 100
+
+Event.create event_name: "seed" event_description: "fun" creator: "god" project_name: "seedproj"
+Project.create name: "seedproj"
+User.create email: "railsdecal@gmail.com" password: "password"
